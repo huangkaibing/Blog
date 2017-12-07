81d254903051d2935756c0ee733e1070465be190
huangkaibing <huangkb_vip@qq.com>
2017-12-07 23:29:56 +0800

    modify

--- Client/lib/baidu.js
+++ Client/lib/baidu.js
@@ -18,3 +18,8 @@ var _hmt = _hmt || [];
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
 })();
+
+(function () {
+    var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?8a0276610aba9c6773f371f68e6c177a" : "https://jspassport.ssl.qhimg.com/11.0.1.js?8a0276610aba9c6773f371f68e6c177a";
+    document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
